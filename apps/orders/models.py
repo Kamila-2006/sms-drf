9bdb495280b50fb9298d7a035ddca0f58b912b93
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-06-16 22:02:29 +0500

    исправлен баг и выполнены миграции

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -14,12 +14,12 @@ class Order(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length='20', choices=STATUS_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     shipping_address = models.TextField()
     notes = models.TextField(blank=True, null=True)
     
 class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order-items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
     quantity = models.PositiveIntegerField()
     shipping_fee = models.PositiveIntegerField()
     tracking_number = models.CharField(max_length=18)
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+from .models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'phone', 'name', 'email', 'default_shipping_address', 'date_joined']
